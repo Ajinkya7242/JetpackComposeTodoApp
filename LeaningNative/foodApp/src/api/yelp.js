f24281efe29931d4b08f379b011daa3a8e84b511
Ajinkya <ajinkya@bottlecapps.com>
2025-04-20 21:10:43 +0530

    first push

--- LeaningNative/foodApp/src/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses/",
-  headers: {
-    Authorization:
-      "Bearer sQQxJBszyhuVOxdjqf_9GpMb349XR4MNNQr6lTk8iuXEDsDUhbUC1qax-cvVLJ9T07fbr6qSxI8m7HpDHbcr-8pw5nFkHmL37O2m_kWvKd1IXD_FqElEAWvA8kzHZ3Yx",
-  },
-});
